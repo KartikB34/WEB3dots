be2a7f2c8f2f563443dbb8231d559f8251ecb3a7
Kartik Barmaiya <kartik.barmaiya28@gmail.com>
2022-10-11 16:15:00 +0530

    schedule animated

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -4,15 +4,21 @@ import phone from "../assets/phone.png"
 import bulb from "../assets/bulb.png"
 import mouse from "../assets/mouse.png"
 import pc from "../assets/pc.png"
+import { motion } from 'framer-motion'
 
 const Landing = () => {
   return (
     <div className='relative text-center h-[76vh] mt-8 flex flex-col justify-center z-10 items-center'>
       <div className=''>
-        <p className=' text-6xl transform transition duration-500 hover:scale-125 relative group md:text-8xl text-transparent bg-clip-text font-extrabold bg-gradient-to-b from-[#C385F1] to-[#7A91D6]'>
-          WEB3DOT
+        <div className='group relative'>
+          <motion.span
+            animate={{}}
+            className='text-6xl md:text-8xl text-transparent bg-clip-text font-extrabold bg-gradient-to-b from-[#C385F1] to-[#7A91D6]'
+          >
+            WEB3DOT
+          </motion.span>
           {/* <div className="absolute top-0 -inset-full h-full w-1/2 z-5 block transform -skew-x-12 bg-gradient-to-r from-transparent to-white opacity-40 group-hover:animate-shine" /> */}
-        </p>
+        </div>
       </div>
       <p className='text-3xl my-16'>Connecting dots in Web3</p>
       <a href='/' className='z-10 transform transition duration-500 hover:scale-125 h-12 flex border border-[#EB7EF1] rounded-lg items-center w-32 justify-center'>
--- src/components/schedule/Schedule.jsx
+++ src/components/schedule/Schedule.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import Aos from 'aos'
+import "aos/dist/aos.css"
 
 const Schedule = () => {
+
+    useEffect(() => {
+        Aos.init({duration: 800});
+    },[])
+    
   return (
     <div className='px-12 pt-20 pb-8'>
       <p className='text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#C385F1] to-[#7A91D6]'>SCHEDULE</p>
@@ -13,9 +20,9 @@ const Schedule = () => {
       <div className='flex justify-center mt-20'>
         {/* Left div */}
         <div className='hidden md:block'>
-            <div className='w-[335px] md:w-[400px] h-20 mb-8 text-right'><p>19th July 2021</p></div>
+            <div data-Aos="fade-up" className='w-[335px] md:w-[400px] h-20 mb-8 text-right'><p>19th July 2021</p></div>
 
-            <div className='p-4 relative mb-8 flex flex-col w-[335px] md:w-[400px]  bg-[#242424]'>
+            <div data-Aos="fade-up" className='p-4 relative mb-8 flex flex-col w-[335px] md:w-[400px]  bg-[#242424]'>
                 <div className='bg-gradient-to-b w-1 h-36 absolute right-0 top-0 from-[#C385F1] to-[#7A91D6]'></div>
                 <div className='flex'>
                     <p className='mr-4 text-transparent bg-clip-text bg-gradient-to-r from-[#C385F1] to-[#7A91D6] font-bold'>4:00pm-5:00pm</p>
@@ -34,9 +41,9 @@ const Schedule = () => {
                 </div>
             </div>
 
-            <div className='w-[335px] md:w-[400px] h-20 mb-8 text-right'><p>19th July 2021</p></div>
+            <div data-Aos="fade-up" className='w-[335px] md:w-[400px] h-20 mb-8 text-right'><p>19th July 2021</p></div>
 
-            <div className='p-4 relative flex w-[335px] md:w-[400px] mb-8 bg-[#242424]'>
+            <div data-Aos="fade-up" className='p-4 relative flex w-[335px] md:w-[400px] mb-8 bg-[#242424]'>
                 <div className='bg-gradient-to-b w-1 h-20 absolute right-0 top-0 from-[#C385F1] to-[#7A91D6]'></div>
                 <p className='mr-4 text-transparent bg-clip-text bg-gradient-to-r from-[#C385F1] to-[#7A91D6] font-bold'>4:00pm-5:00pm</p>
                 <div>
@@ -47,7 +54,7 @@ const Schedule = () => {
 
             <div className='w-[335px] md:w-[400px] h-36 mb-8 text-left'></div>
 
-            <div className='p-4 relative mb-8 flex flex-col w-[335px] md:w-[400px]  bg-[#242424]'>
+            <div data-Aos="fade-up" className='p-4 relative mb-8 flex flex-col w-[335px] md:w-[400px]  bg-[#242424]'>
                 <div className='bg-gradient-to-b w-1 h-36 absolute right-0 top-0 from-[#C385F1] to-[#7A91D6]'></div>
                 <div className='flex'>
                     <p className='mr-4 text-transparent bg-clip-text bg-gradient-to-r from-[#C385F1] to-[#7A91D6] font-bold'>4:00pm-5:00pm</p>
@@ -72,14 +79,14 @@ const Schedule = () => {
 
         <div className='px-4 md:px-8 relative'>
             <div className='h-[67.5rem] md:h-[63rem] w-[7px]  rounded-full bg-gradient-to-r from-[#C385F1] to-[#7A91D6]'>
-                <div className='h-[21px] w-[21px] absolute right-[10px] md:right-[25px] bg-[#7A91D6] rounded-full'></div>
-                <div className='h-[21px] w-[21px] absolute right-[10px] md:right-[25px] top-[8.5rem] md:top-28 bg-[#7A91D6] rounded-full'></div>
-                <div className='h-[21px] w-[21px] absolute right-[10px] md:right-[25px] top-[19.5rem] md:top-64 mt-6 bg-[#7A91D6] rounded-full'></div>
-                <div className='h-[21px] w-[21px] absolute right-[10px] md:right-[25px] top-[29rem] md:top-[25rem] bg-[#7A91D6] rounded-full'></div>
+                <div data-Aos="fade-up" className='h-[21px] w-[21px] absolute right-[10px] md:right-[25px] bg-[#7A91D6] rounded-full'></div>
+                <div data-Aos="fade-up" className='h-[21px] w-[21px] absolute right-[10px] md:right-[25px] top-[8.5rem] md:top-28 bg-[#7A91D6] rounded-full'></div>
+                <div data-Aos="fade-up" className='h-[21px] w-[21px] absolute right-[10px] md:right-[25px] top-[19.5rem] md:top-64 mt-6 bg-[#7A91D6] rounded-full'></div>
+                <div data-Aos="fade-up" className='h-[21px] w-[21px] absolute right-[10px] md:right-[25px] top-[29rem] md:top-[25rem] bg-[#7A91D6] rounded-full'></div>
 
-                <div className='h-[21px] w-[21px] absolute right-[10px] md:right-[25px] bottom-[32rem] bg-[#7A91D6] rounded-full'></div>
-                <div className='h-[21px] w-[21px] absolute right-[10px] md:right-[25px] bottom-[21rem] bg-[#7A91D6] rounded-full'></div>
-                <div className='h-[21px] w-[21px] absolute right-[10px] md:right-[25px] bottom-40 bg-[#7A91D6] rounded-full'></div>
+                <div data-Aos="fade-up" className='h-[21px] w-[21px] absolute right-[10px] md:right-[25px] bottom-[32rem] bg-[#7A91D6] rounded-full'></div>
+                <div data-Aos="fade-up" className='h-[21px] w-[21px] absolute right-[10px] md:right-[25px] bottom-[21rem] bg-[#7A91D6] rounded-full'></div>
+                <div data-Aos="fade-up" className='h-[21px] w-[21px] absolute right-[10px] md:right-[25px] bottom-40 bg-[#7A91D6] rounded-full'></div>
             </div>
         </div>
 
@@ -87,9 +94,9 @@ const Schedule = () => {
         {/*Right div */}
         <div>
 
-            <div className='w-[335px] md:w-[400px] md:hidden text-left'><p>19th July 2021</p></div>
+            <div data-Aos="fade-up" className='w-[335px] md:w-[400px] md:hidden text-left'><p>19th July 2021</p></div>
 
-            <div className='p-4 relative flex w-[335px] md:w-[400px] mb-8 bg-[#242424]'>
+            <div data-Aos="fade-up" className='p-4 relative flex w-[335px] md:w-[400px] mb-8 bg-[#242424]'>
                 <div className='bg-gradient-to-b w-1 h-20 absolute left-0 top-0 from-[#C385F1] to-[#7A91D6]'></div>
                 <p className='mr-4 text-transparent bg-clip-text bg-gradient-to-r from-[#C385F1] to-[#7A91D6] font-bold'>4:00pm-5:00pm</p>
                 <div>
@@ -98,9 +105,9 @@ const Schedule = () => {
                 </div>
             </div>
 
-            <div className='w-[335px] md:w-[400px] md:h-36 md:mb-8 text-left'><p>19th July 2021</p></div>
+            <div data-Aos="fade-up" className='w-[335px] md:w-[400px] md:h-36 md:mb-8 text-left'><p>19th July 2021</p></div>
             
-            <div className='p-4 md:hidden relative mb-8 flex flex-col w-[335px] md:w-[400px] bg-[#242424]'>
+            <div data-Aos="fade-up" className='p-4 md:hidden relative mb-8 flex flex-col w-[335px] md:w-[400px] bg-[#242424]'>
                 <div className='bg-gradient-to-b w-1 h-36 absolute left-0 top-0 from-[#C385F1] to-[#7A91D6]'></div>
                 <div className='flex'>
                     <p className='mr-4 text-transparent bg-clip-text bg-gradient-to-r from-[#C385F1] to-[#7A91D6] font-bold'>4:00pm-5:00pm</p>
@@ -119,9 +126,9 @@ const Schedule = () => {
                 </div>
             </div>
 
-            <div className='w-[335px] md:w-[400px] md:hidden text-left'><p>19th July 2021</p></div>
+            <div data-Aos="fade-up" className='w-[335px] md:w-[400px] md:hidden text-left'><p>19th July 2021</p></div>
 
-            <div className='p-4 relative flex w-[335px] md:w-[400px] mb-8 bg-[#242424]'>
+            <div data-Aos="fade-up" className='p-4 relative flex w-[335px] md:w-[400px] mb-8 bg-[#242424]'>
                 <div className='bg-gradient-to-b w-1 h-20 absolute left-0 top-0 from-[#C385F1] to-[#7A91D6]'></div>
                 <p className='mr-4 text-transparent bg-clip-text bg-gradient-to-r from-[#C385F1] to-[#7A91D6] font-bold'>4:00pm-5:00pm</p>
                 <div>
@@ -130,7 +137,7 @@ const Schedule = () => {
                 </div>
             </div>
 
-            <div className='p-4 relative md:hidden flex w-[335px] md:w-[400px] mb-8 bg-[#242424]'>
+            <div data-Aos="fade-up" className='p-4 relative md:hidden flex w-[335px] md:w-[400px] mb-8 bg-[#242424]'>
                 <div className='bg-gradient-to-b w-1 h-20 absolute left-0 top-0 from-[#C385F1] to-[#7A91D6]'></div>
                 <p className='mr-4 text-transparent bg-clip-text bg-gradient-to-r from-[#C385F1] to-[#7A91D6] font-bold'>4:00pm-5:00pm</p>
                 <div>
@@ -141,7 +148,7 @@ const Schedule = () => {
 
             <div className='w-[335px] md:w-[400px] hidden md:block h-20 mb-8 text-left'></div>
 
-            <div className='p-4 relative mb-8 flex flex-col w-[335px] md:w-[400px]  bg-[#242424]'>
+            <div data-Aos="fade-up" className='p-4 relative mb-8 flex flex-col w-[335px] md:w-[400px]  bg-[#242424]'>
                 <div className='bg-gradient-to-b w-1 h-36 absolute left-0 top-0 from-[#C385F1] to-[#7A91D6]'></div>
                 <div className='flex'>
                     <p className='mr-4 text-transparent bg-clip-text bg-gradient-to-r from-[#C385F1] to-[#7A91D6] font-bold'>4:00pm-5:00pm</p>
@@ -163,7 +170,7 @@ const Schedule = () => {
 
             <div className='w-[335px] md:w-[400px] hidden md:block h-36 mb-8 text-left'></div>
 
-            <div className='p-4 md:hidden relative mb-8 flex flex-col w-[335px] md:w-[400px] bg-[#242424]'>
+            <div data-Aos="fade-up" className='p-4 md:hidden relative mb-8 flex flex-col w-[335px] md:w-[400px] bg-[#242424]'>
                 <div className='bg-gradient-to-b w-1 h-36 absolute left-0 top-0 from-[#C385F1] to-[#7A91D6]'></div>
                 <div className='flex'>
                     <p className='mr-4 text-transparent bg-clip-text bg-gradient-to-r from-[#C385F1] to-[#7A91D6] font-bold'>4:00pm-5:00pm</p>
@@ -182,7 +189,7 @@ const Schedule = () => {
                 </div>
             </div>
 
-            <div className='p-4 relative mb-8 flex flex-col w-[335px] md:w-[400px]  bg-[#242424]'>
+            <div data-Aos="fade-up" className='p-4 relative mb-8 flex flex-col w-[335px] md:w-[400px]  bg-[#242424]'>
                 <div className='bg-gradient-to-b w-1 h-36 absolute left-0 top-0 from-[#C385F1] to-[#7A91D6]'></div>
                 <div className='flex'>
                     <p className='mr-4 text-transparent bg-clip-text bg-gradient-to-r from-[#C385F1] to-[#7A91D6] font-bold'>4:00pm-5:00pm</p>
